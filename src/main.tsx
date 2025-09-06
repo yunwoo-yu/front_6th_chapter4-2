c05158517d27f20ada8002ff43e66619783adc3f
junilhwang <junil.h@kakao.com>
2025-09-06 16:03:45 +0900

    feat: Strict 모드 제거 (편의를 위함)

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
+createRoot(document.getElementById("root")!).render(
+  <ChakraProvider>
+    <App/>
+  </ChakraProvider>
 )
