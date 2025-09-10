bf1fa5c655f669bbf7e768d3ea9e9a673397d198
Ycow <skypnal12@gmail.com>
2025-09-10 20:13:11 +0900

    fix: useAutoCallback type fix

--- src/hooks/useAutoCallback.tsx
+++ src/hooks/useAutoCallback.tsx
@@ -1,13 +1,13 @@
 import { useCallback, useRef } from "react";
 
-export const useAutoCallback = <T extends (...args: any[]) => void>(
+export const useAutoCallback = <T extends (...args: any[]) => any>(
   callback: T
 ) => {
   const ref = useRef(callback);
 
   ref.current = callback;
 
-  return useCallback((...args: Parameters<T>) => {
-    ref.current(...args);
+  return useCallback((...args: Parameters<T>): ReturnType<T> => {
+    return ref.current(...args);
   }, []);
 };
